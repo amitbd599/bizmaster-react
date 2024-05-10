e348f36c0ee438b78d28e792765e443942b3f0b1
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2024-05-10 22:42:49 +0600

    add new rtl

---
+++ master-rtl/src/components/AboutOne.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const AboutOne = () => {
+  return (
+    <div className="about-area-1 position-relative space-top">
+      <div className="about1-shape-img1">
+        <img
+          className="about1-shape-img-1"
+          src="assets/img/normal/about_shape1-1.jpg"
+          alt="img"
+        />
+      </div>
+      <div className="about1-shape-img2">
+        <img
+          className="about1-shape-img-2"
+          src="assets/img/normal/about_shape1-2.png"
+          alt="img"
+        />
+      </div>
+      <div className="container">
+        <div className="row gx-60 align-items-center">
+          <div className="col-xl-6">
+            <div className="about-content-wrap">
+              <div className="title-area me-xl-5 mb-20">
+                <span className="sub-title">
+                  <img src="assets/img/icon/title_left.svg" alt="shape" />
+                  حولنا
+                </span>
+                <h2 className="sec-title">أحدث خدمة أحدث خدمة أحدث خدمة</h2>
+                <p className="sec-text mb-35">
+                  أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة
+
+
+                </p>
+              </div>
+              <div className="achive-about">
+                <div className="achive-about_content">
+                  <div className="achive-about_icon">
+                    <i className="fas fa-check" />
+                  </div>
+                  <div className="media-body">
+                    <h3 className="box-title">حلول استراتيجية محترفة</h3>
+                    <p className="achive-about_text">
+                      هأحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="achive-about">
+                <div className="achive-about_content">
+                  <div className="achive-about_icon">
+                    <i className="fas fa-check" />
+                  </div>
+                  <div className="media-body">
+                    <h3 className="box-title">شركاء تعزيز الأداء</h3>
+                    <p className="achive-about_text">
+                      هأحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة أحدث خدمة
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="btn-wrap mt-20">
+                <Link to="/about" className="global-btn mt-xl-0 mt-20">
+                  اقرأ المزيد <i className="fas fa-arrow-right ms-2" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
+export default AboutOne;
---
+++ master-rtl/src/components/ServiceOne.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ServiceOne = () => {
+  return (
+    <div className="service-area-1 space overflow-hidden">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-xl-7">
+            <div className="title-area text-center">
+              <span className="sub-title">
+                <img src="assets/img/icon/title_left.svg" alt="shape" />
+                أحدث الخدمات
+              </span>
+              <h2 className="sec-title style2">
+                تمكين الأعمال للتميز
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row gx-30 gy-30 justify-content-center">
+          <div className="col-md-6">
+            <div className="service-card">
+              <div className="service-card_img">
+                <img src="assets/img/service/service-1-1.jpg" alt="img" />
+              </div>
+              <div className="service-card_content">
+                <h4 className="service-card_title">
+                  <Link to="/service-details">خبراء الكفاءة</Link>
+                </h4>
+                <p className="service-card_text">
+                  العديد من حزم تحرير صفحات الويب الآن تستخدم Lorem Ipsum كنموذج نص افتراضي، وبحثًا
+                </p>
+                <Link to="/service-details" className="link-btn">
+                  قراءة المزيد <i className="fas fa-arrow-right" />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="service-card">
+              <div className="service-card_img">
+                <img src="assets/img/service/service-1-2.jpg" alt="img" />
+              </div>
+              <div className="service-card_content">
+                <h4 className="service-card_title">
+                  <Link to="/service-details">إتقان الإدارة</Link>
+                </h4>
+                <p className="service-card_text">
+                  العديد من حزم تحرير صفحات الويب الآن تستخدم Lorem Ipsum كنموذج نص افتراضي، وبحثًا
+                </p>
+                <Link to="/service-details" className="link-btn">
+                  قراءة المزيد <i className="fas fa-arrow-right" />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="service-card">
+              <div className="service-card_img">
+                <img src="assets/img/service/service-1-3.jpg" alt="img" />
+              </div>
+              <div className="service-card_content">
+                <h4 className="service-card_title">
+                  <Link to="/service-details">مسرعات النجاح</Link>
+                </h4>
+                <p className="service-card_text">
+                  العديد من حزم تحرير صفحات الويب الآن تستخدم Lorem Ipsum كنموذج نص افتراضي، وبحثًا
+                </p>
+                <Link to="/service-details" className="link-btn">
+                  قراءة المزيد <i className="fas fa-arrow-right" />
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="service-card">
+              <div className="service-card_img">
+                <img src="assets/img/service/service-1-4.jpg" alt="img" />
+              </div>
+              <div className="service-card_content">
+                <h4 className="service-card_title">
+                  <Link to="/service-details">النمو والابتكار</Link>
+                </h4>
+                <p className="service-card_text">
+                  العديد من حزم تحرير صفحات الويب الآن تستخدم Lorem Ipsum كنموذج نص افتراضي، وبحثًا
+                </p>
+                <Link to="/service-details" className="link-btn">
+                  قراءة المزيد <i className="fas fa-arrow-right" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
+};
+
+export default ServiceOne;
